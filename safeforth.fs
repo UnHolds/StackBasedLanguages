55173a0e861580ed67ec5c7d864d424ae474fdaa
UnHold (Alex) <alex@hold.re>
2024-12-15 21:42:32 +0100

    changed safe forth

--- safeforth.fs
+++ safeforth.fs
@@ -1,25 +1,21 @@
+
+: str-synonym1 ( c-addr u -- )
+  2dup nextname ['] synonym execute-parsing ;
+
+: copy-names ( "name1 ..." -- )
+  begin
+    parse-name dup while
+      str-synonym1
+  repeat
+  2drop ;
+
+
 wordlist constant safe-wordlist \ create new wordlist
 safe-wordlist set-current \ set the compilation wordlist as the new wordlist
-get-order safe-wordlist swap 1 + set-order \ add the new wordlist to the wordlists (make it searchable)
+safe-wordlist >order \ add the new wordlist to the wordlists (make it searchable)
 
-: def-word-by-str ( addr op addr name -- )
-        nextname
-        find-name name>comp drop \ TODO: add error handeling
-        CREATE ,
-    DOES> ( executes xt )
-        s" new definition" type @ execute ; \ s" new definition" type <- this is just here to show that it works
+\ those names need to exist
+copy-names bye bootmessage
 
-
-\ this should later read the commands from a file
-: safe-def
-    s" .s" 2dup def-word-by-str
-    s" bye" 2dup def-word-by-str
-    s" dup" 2dup def-word-by-str
-;
-
-safe-def \ execute the safe def
-
-\ remove all the other wordslist
+\ remove all other save lists
 safe-wordlist 1 set-order
-
-\ now the only available commands are [.s, bye, dup]
