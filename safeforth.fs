e9e1cd0cd3b7b0fd27e7a0a87cfdfc2d8fbf977e
UnHold (Alex) <alex@hold.re>
2024-12-17 17:18:06 +0100

    added - read cmd from file

--- safeforth.fs
+++ safeforth.fs
@@ -1,4 +1,9 @@
 
+\ for reading files
+0 Value fd-in
+256 Constant max-line
+Create line-buffer  max-line 2 + allot
+
 : str-synonym1 ( c-addr u -- )
   2dup nextname ['] synonym execute-parsing ;
 
@@ -9,12 +14,32 @@
   repeat
   2drop ;
 
+: copy-names-from-file ( addr u -- )
+  r/o open-file throw ( file ptr ) to fd-in
+  begin
+    line-buffer max-line fd-in read-line throw
+  while
+    line-buffer swap str-synonym1
+  repeat
+
+;
+
+: copy-from-all-arg-files ( ... addr u -- )
+  depth 2 / 0 u+do \ loop through the number of strings on the stack1
+    copy-names-from-file
+    drop
+  loop
+
+;
+
+
 
 wordlist constant safe-wordlist \ create new wordlist
 safe-wordlist set-current \ set the compilation wordlist as the new wordlist
 safe-wordlist >order \ add the new wordlist to the wordlists (make it searchable)
 
 \ those names need to exist
+copy-from-all-arg-files
 copy-names bye bootmessage
 
 \ remove all other save lists
